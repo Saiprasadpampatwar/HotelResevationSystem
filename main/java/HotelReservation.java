038ff773dabc7edcabf2c5c27bad95069b699dbc
SaiprasadPampatwar <pampatwarsaiprasad321@gmail.com>
2020-12-05 10:18:35 +0530

    [Saiprasad]Added: method add hotel in a hotel reservation system

--- main/java/HotelReservation.java
+++ main/java/HotelReservation.java
@@ -1,6 +1,18 @@
+import java.util.HashMap;
+
 public class HotelReservation {
+
+    public static HashMap<String,Hotel> listOfHotels = new HashMap<>();
 
     public static void main(String[] args) {
         System.out.println("Welcome to Hotel Reservation System");
+
+    }
+
+    public void add(String hotelName, int rateRegular, int regularWeekend) {
+        Hotel hotel = new Hotel(hotelName,rateRegular,regularWeekend);
+        listOfHotels.put(hotelName,hotel);
     }
 }
+
+
---
+++ test/java/HotelReservationTest.java
@@ -0,0 +1,18 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class HotelReservationTest {
+    @Test
+    public void givenHotelNameAndRatesShouldReturnTrue() {
+        HotelReservation hotelReservation = new HotelReservation();
+        hotelReservation.add("BridgeWood",160,60);
+        hotelReservation.add("LikeWood",110,90);
+        hotelReservation.add("RidgeWood",220,150);
+        Assert.assertEquals(160,HotelReservation.listOfHotels.get("BridgeWood").getRateRegularCustomer());
+        Assert.assertEquals(60,HotelReservation.listOfHotels.get("BridgeWood").getRateRegularWeekend());
+        Assert.assertEquals(110,HotelReservation.listOfHotels.get("LikeWood").getRateRegularCustomer());
+        Assert.assertEquals(90,HotelReservation.listOfHotels.get("LikeWood").getRateRegularWeekend());
+        Assert.assertEquals(220,HotelReservation.listOfHotels.get("RidgeWood").getRateRegularCustomer());
+        Assert.assertEquals(150,HotelReservation.listOfHotels.get("RidgeWood").getRateRegularWeekend());
+    }
+}
